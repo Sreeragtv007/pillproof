61ac6ca39476495513716866812e2e3e9b08a658
sreerag <sreeragtv007@gmail.com>
2025-09-25 11:18:58 +0530

    test

--- verify/views.py
+++ verify/views.py
@@ -12,24 +12,20 @@ from .agent import verify_prescription_crew
 
 def upload_view(request):
     if request.method == 'POST':
-        form = UploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Save the form data to the database, including the image files
-            verification_entry = form.save(commit=False)
+        
+        
+        priscription = request.FILE.get('priscription')
+        medicine = request.FILE.get('medicine')
+        
+        obj = PrescriptionVerification.objects.create(prescription_image=priscription,medicine_image=medicine)
+        
+        
+        
+        
             
-            # CrewAI Integration
-            # The files are now saved, you can access their paths
-            prescription_path = verification_entry.prescription_image.path
-            medicine_path = verification_entry.medicine_image.path
             
-            # Run the CrewAI verification
-            result = verify_prescription_crew(prescription_path, medicine_path)
             
-            # Save the result to the model instance before committing
-            verification_entry.verification_result = result
-            verification_entry.save()
-            
-            return redirect('result_view', result=result)
+        return redirect('result_view',)
     else:
         form = UploadForm()
     
