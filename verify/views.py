c909ed67297dfdf26a2a9e75b3899d5af8a21ab2
sreerag <sreeragtv007@gmail.com>
2025-09-30 11:45:48 +0530

    ocr_implimented

--- verify/views.py
+++ verify/views.py
@@ -5,16 +5,23 @@ import PIL
 from .models import PrescriptionVerification
 from .gemini_verify import verify_medicine_with_prescription
 import json
-
+from datetime import datetime
+import pytesseract
 
 def upload_view(request):
     if request.method == 'POST':
+        start = datetime.now()
 
         priscription = request.FILES['priscription']
         medicine = request.FILES['medicine']
+        
+       
 
         prescription_img = PIL.Image.open(priscription)
         medicine_img = PIL.Image.open(medicine)
+        
+        text = pytesseract.image_to_string(medicine_img)
+        print(text)
 
         result = verify_medicine_with_prescription(
             prescription_img, medicine_img)
@@ -23,6 +30,8 @@ def upload_view(request):
         print(result)
 
         context = {'result': result}
+        end = datetime.now()
+        print(end-start)
 
         return render(request,'result.html',context)
     else:
