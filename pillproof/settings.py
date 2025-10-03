b108511a701179bff466b6cecf4d381aad385f32
sreerag <sreeragtv007@gmail.com>
2025-10-03 21:08:12 +0530

    test

--- pillproof/settings.py
+++ pillproof/settings.py
@@ -129,8 +129,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
